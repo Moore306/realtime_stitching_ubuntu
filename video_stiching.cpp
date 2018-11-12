291117d3f7224c58283e51d46921381a37190946
orange <yangyanglan1@163.com>
2018-11-12 14:19:25 +0800

    add orb stiching 0.6s

--- video_stiching.cpp
+++ video_stiching.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
 #include "opencv2/opencv.hpp" 
 #include <opencv2/features2d/features2d.hpp>
-//#include<ldb>
-
+#include <chrono>
 using namespace std;
 using namespace cv;
+void regis(Mat& img1,Mat& img2,vector<Point2f> obj,vector<Point2f> scene,Mat &H);
+int main()
+{
+   
+  // 配置图像路径
+    Mat img11 = imread("../c.png");
+    Mat img22 = imread("../d.png");
+    Mat img1,img2;
+    while(true)
+    {
+//         if(waitKey(1) >= 0)
+//  	        break;
+	cvtColor(img11,img1,CV_BGR2GRAY);
+	cvtColor(img22,img2,CV_BGR2GRAY);
+	// 判断输入图像是否读取成功
+	if (img1.empty() || img2.empty() || img1.channels() != 1 || img2.channels() != 1)
+	{
+	    cout << "Input Image is nullptr or the image channels is not gray!" << endl;
+	    system("pause");
+	}
+	vector<Point2f> kps1,kps2;
+	Mat H;
+	chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+	regis(img1,img2,kps1,kps2,H);
+	cout<<H<<endl;
+    
+	//拼接图像
+	Mat tiledImg;
+	Mat shftMat=(Mat_<double>(3,3)<<1.0,0,img1.cols, 0,1.0,0, 0,0,1.0);
+	warpPerspective(img11,tiledImg,shftMat*H,Size(img1.cols+img2.cols,img2.rows));
+	img22.copyTo(Mat(tiledImg,Rect(img1.cols,0,img2.cols,img2.rows)));
+	chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+	chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>( t2-t1 );
+	cout<<"LK Flow use time："<<time_used.count()<<" seconds."<<endl;
+	//imwrite("tied.jpg",tiledImg);
+	imshow("result",tiledImg);
+    }
+}
 
-int main(void)
+int main2(void)
 {
     // 配置图像路径
     Mat img1 = imread("../c.png", 0);
@@ -49,12 +86,14 @@ int main(void)
         if (matches[i][0].distance < 0.8*matches[i][1].distance)
         {
             goodMatchKpts.push_back(matches[i][0]);
+	    //cout<<matches[i][0]<<endl;
         }
     }
     // 显示匹配点对
     Mat show_match;
+    cout<<kpts1.size()<<" "<<kpts2.size()<<endl;
     drawMatches(img1, kpts1, img2, kpts2, goodMatchKpts, show_match);
-
+    cout<<kpts1.size()<<" "<<kpts2.size()<<endl;
     // 显示输出
     ostringstream s_time;
     s_time << time;
@@ -98,6 +137,71 @@ int main(void)
     return 0;
 }
 
+void regis(Mat& img1,Mat& img2,vector<Point2f> obj,vector<Point2f> scene,Mat &H)
+{
+    obj.clear();
+    scene.clear();
+    // ORB算法继承Feature2D基类
+    Ptr<ORB> orb = ORB::create(1000, 1.2, 8, 31, 0, 2, 0, 31, 20);  
+    // 调整精度，值越小点越少，越精准
+    vector<KeyPoint> kpts1, kpts2;
+    // 特征点检测算法...
+    orb->detect(img1, kpts1);
+    orb->detect(img2, kpts2);
+
+    // 特征点描述算法...
+    Mat desc1, desc2;
+
+    bool SelectiveDescMethods = true;
+    // 默认选择BRIEF描述符
+    if (SelectiveDescMethods) 
+    {
+        // ORB 算法中默认BRIEF描述符
+        orb->compute(img1, kpts1, desc1);
+        orb->compute(img2, kpts2, desc2);
+    }
+ 
+    // 粗精匹配数据存储结构
+    vector< vector<DMatch>> matches;
+    vector<DMatch> goodMatchKpts;
+    // Keypoint Matching...
+    DescriptorMatcher *pMatcher = new BFMatcher(NORM_HAMMING, false);
+    pMatcher->knnMatch(desc1, desc2, matches, 2);
+    // 欧式距离度量  阈值设置为0.8
+    for (unsigned int i = 0; i < matches.size(); ++i)
+    {
+        if (matches[i][0].distance < 0.8*matches[i][1].distance)
+        {
+            goodMatchKpts.push_back(matches[i][0]);
+	   
+        }
+    }
+    cout << "(kpts1: " << kpts1.size() << ") && (kpts2:" \
+         << kpts2.size() << ") = goodMatchesKpts: " << goodMatchKpts.size() << endl;
+
+    waitKey(0);
+
+    // RANSAC Geometric Verification
+    if (goodMatchKpts.size() < 4)
+    {
+        cout << "The Match Kpts' Size is less than Four to estimate!" << endl;
+        return;
+    }
+
+    
+    for (unsigned int i = 0; i < goodMatchKpts.size(); ++i)
+    {
+        obj.push_back(kpts1[goodMatchKpts[i].queryIdx].pt);
+        scene.push_back(kpts2[goodMatchKpts[i].trainIdx].pt);
+    }
+    // 估计Two Views变换矩阵
+    H = findHomography(obj, scene, CV_RANSAC);
+    vector<Point2f> obj_corners(4), scene_corners(4);
+    obj_corners[0] = cvPoint(0, 0); obj_corners[1] = cvPoint(img1.cols, 0);
+    obj_corners[2] = cvPoint(img1.cols, img1.rows); obj_corners[3] = cvPoint(0, img1.rows);
+    // 点集变换标出匹配重复区域
+    //perspectiveTransform(obj_corners, scene_corners, H);
+}
 
 // #include <iostream>
 // #include <fstream>
